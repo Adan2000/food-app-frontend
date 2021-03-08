706d81498a263e4741fdc34a38edea9119ff93a5
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-07 19:42:29 -0800

    simple html form

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ const BASE_URL = "http://localhost:3000"
 const USER_URL = `${BASE_URL}/users`
 const REVIEW = `${BASE_URL}/reviews`
 
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    
+})
