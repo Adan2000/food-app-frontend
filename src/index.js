65a84354a90bb86988c842015e43cd7084ed752a
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-09 10:52:41 -0800

    got data to console

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const BASE_URL = "http://localhost:3000"
-const USER_URL = `${BASE_URL}/users`
-const REVIEW = `${BASE_URL}/reviews`
+const USER_URL = `http://localhost:3000/users`
+const REVIEW = `http://localhost:3000/reviews`
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    getReviews()
 
-    
+    function getReviews(){
+        fetch(REVIEW)
+        .then(resp => resp.json())
+        .then(resp => console.log(resp))
+    }
+     
+    const submitForm = document.querySelector('.submit')
+
+    // submitForm.addEventListener('submit', function(e){
+    //     e.preventDefault()
+    //     let review = {
+    //         "user": review.target.name
+    //     }
+    // })
+
+
 })
+
