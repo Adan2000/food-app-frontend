789487f711865e8ea5ca50314a8c905b20e0a6f3
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-09 17:31:17 -0800

    POST

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
 const BASE_URL = "http://localhost:3000"
 const USER_URL = `http://localhost:3000/users`
 const REVIEW = `http://localhost:3000/reviews`
+let currentUser = {id:1}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
     getReviews()
 
+
+z
     function getReviews(){
         fetch(REVIEW)
         .then(resp => resp.json())
         .then(resp => console.log(resp))
     }
      
-    const submitForm = document.querySelector('.submit')
+    //selecting form
+    let submitForm = document.querySelector('.submit')
+    //adding event listener
+    submitForm.addEventListener('submit',submitF)
+    
+    //setting the value
+    function submitF(e){
+        e.preventDefault()
+        let review = {
+            "restaurant_name": e.target.restaurant.value,
+            "food_name": e.target.food.value,
+            "food_review": e.target.review.value, 
+            "user_id": currentUser.id 
+        }
+        console.log(review)
+        postReview(review)
+    }
 
-    // submitForm.addEventListener('submit', function(e){
-    //     e.preventDefault()
-    //     let review = {
-    //         "user": review.target.name
-    //     }
-    // })
-
+    //POST
+    function postReview(review){
+        fetch(REVIEW, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({review})//same as {review:review}
+        })
+        .then(res => res.json())
+        .then(console.log)
+        .catch(error => console.log(error))
+    }
 
 })
+
 
